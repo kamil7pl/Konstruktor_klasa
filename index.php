c131a5a3067886b49204152e62885caa56ee7683
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-02-05 15:15:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Konstruktor</title>
+</head>
+<body>
+    <?php
+	require_once("konstruktor.php");
+		$k=new konstruktor();
+		$ko=new konstruktor(41);
+		
+		
+		?>
+</body>
+</html>
---
+++ konstruktor.php
@@ -0,0 +1,8 @@
+<?php
+	class konstruktor{
+		function __construct($a=5){
+			echo $a."<br />";
+		}
+		
+	}
+?>
